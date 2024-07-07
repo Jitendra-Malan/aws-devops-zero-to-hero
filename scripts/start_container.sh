af7084fc9dadfe6460df45955fd071a042e7707a
Jitendra-Malan <165754484+Jitendra-Malan@users.noreply.github.com>
2024-07-07 15:36:16 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push jitendramalan/simple-python-flask-app:latest
+docker pull jitendramalan/simple-python-flask-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 jitendramalan/simple-python-flask-app
